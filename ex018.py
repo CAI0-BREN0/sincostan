38755d67f91b8573498a04b4caecd81fcdfc61a7
Caio Breno <89470026+CAI0-BREN0@users.noreply.github.com>
2023-05-16 21:05:09 -0400

    Add files via upload

---
+++ ex018.py
@@ -0,0 +1,3 @@
+import math
+n1 = int(input("Digite um numero "))
+print("o numero {} seu seno è de {}, e seu cosseno é {} e sua tangente{}" .format(n1,math.sin(math.radians(n1)),math.cos(n1),math.tan(math.radians(math.radians(n1)))))
